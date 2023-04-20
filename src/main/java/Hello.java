5f33825bd97e1776eaf47d5b357e1347b1cf381e
beth-gooding <beth.gooding@hotmail.com>
2023-04-20 14:19:35 +0100

    feat: Adds first variable declaration

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,5 +1,15 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello Beth");
+
+        /*
+        * Variable declaration statement
+        * <data type> <variable name> = <value>
+        * It is optional to set the value during the declaration
+        * But in general, variables should be initialised before being used
+        */
+        int myFirstNumber = (10 + 5) + (2 * 10);
+
+        System.out.println(myFirstNumber);
     }
 }
