e12c564a26164b25d73375ed1e0f00d6d1b5560c
beth-gooding <beth.gooding@hotmail.com>
2023-04-25 10:56:39 +0100

    feat: Adds mathematical operators abbreviations

--- src/main/java/FirstStepsJava11/Operators/Operators.java
+++ src/main/java/FirstStepsJava11/Operators/Operators.java
@@ -26,5 +26,25 @@ public class Operators {
         // The % sign is used to do remainders
         result = result % 3; // the remainder of (4 % 3) = 1
         System.out.println("4 % 3 = " + result);
+
+        // Operator abbreviations
+        result++; // Same as result = result + 1
+        System.out.println("1 + 1 = " + result);
+
+        result--; // Same as result = result - 1
+        System.out.println("2 - 1 = " + result);
+
+        result += 2; // Same as result = result + 2
+        System.out.println("1 + 2 = " + result);
+
+        result *= 10; // Same as result = result * 10
+        System.out.println("3 * 10 = " + result);
+
+        result /= 3; // Same as result = result / 3
+        System.out.println("30 / 3 = " + result);
+
+        result -= 2; // Same as result = result -2
+        System.out.println("10 - 2 = " + result);
+
     }
 }
