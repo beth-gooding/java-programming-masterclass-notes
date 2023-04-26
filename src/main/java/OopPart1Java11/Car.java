35378b816d978a71b5ca6efb6f9418c2a6ddc70a
beth-gooding <beth.gooding@hotmail.com>
2023-04-26 16:25:30 +0100

    feat: Implements the Car class using private fields only

---
+++ src/main/java/OopPart1Java11/Car.java
@@ -0,0 +1,15 @@
+package OopPart1Java11;
+
+// Classes should have capital letters to start their names
+// Public access modifier means any other classes have access to this class
+public class Car {
+    // As a general rule, we use the private access modifier for fields/methods inside classes
+    // This follows the principle of encapsulation, so that there is no outside access to the
+    // internal fields of a class
+
+    private int doors;
+    private int wheels;
+    private String model;
+    private String engine;
+    private String colour;
+}
---
+++ src/main/java/OopPart1Java11/Main.java
@@ -0,0 +1,10 @@
+package OopPart1Java11;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // created a new object called ferrari of type Car
+        Car ferrari = new Car();
+        Car redBull = new Car();
+    }
+}
